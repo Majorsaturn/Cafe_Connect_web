1abaf3059ec2176958f3dfd1cf8bb996f54a5e95
Majorsaturn <nicoanovak@gmail.com>
2024-10-05 13:18:18 -0400

    rough skeleton

--- index.js
+++ index.js
@@ -5,29 +5,30 @@ var server = http.createServer(function (req, res) {
 
 
     if (req.url == '/') {
-
+        // res.write('<html><body><p>Comp Sci.</p></body></html>');
         //response header
         if(req.method == "GET"){
             res.writeHead(200, {'Content-Type': 'text/html'});
-            res.end('<h1>GET, Method!</h1>')
+            res.end('<h1>This is the Home Page</h1>')
         }
         else if(req.method == "POST"){
             res.writeHead(200, {'Content-Type': 'text/html'});
-            res.end('<h1>POST, Method!</h1>')
+            res.end('<h1>you entered something on the home page</h1>')
         }
     }
-    else if(req.url == "/student"){
+    else if(req.url == "/login"){
+        // res.write('<html><body><p>Comp Sci.</p></body></html>');
         //response header
         if(req.method == "GET"){
             res.writeHead(200, {'Content-Type': 'text/html'});
-            res.end('<h1>GET, Method!</h1>')
+            res.end('<h1>Login Screen</h1>')
         }
         else if(req.method == "POST"){
             res.writeHead(200, {'Content-Type': 'text/html'});
-            res.end('<h1>POST, Method!</h1>')
+            res.end('<h1>Login Enter</h1>')
         }
     }
-    else if(req.url == "/major"){
+    else if(req.url == "/signup"){
         // res.write('<html><body><p>Comp Sci.</p></body></html>');
         //response header
         if(req.method == "GET"){
@@ -39,7 +40,7 @@ var server = http.createServer(function (req, res) {
             res.end('<h1>POST, Method!</h1>')
         }
     }
-    else if(req.url == "/Education"){
+    else if(req.url == "/user/profile/settings/profilesettings"){
         // res.write('<html><body><p>Comp Sci.</p></body></html>');
         //response header
         if(req.method == "GET"){
@@ -49,6 +50,206 @@ var server = http.createServer(function (req, res) {
         else if(req.method == "POST"){
             res.writeHead(200, {'Content-Type': 'text/html'});
             res.end('<h1>POST, Method!</h1>')
+        }
+    }
+    else if(req.url == "/profile/settings/deleteuser"){
+        // res.write('<html><body><p>Comp Sci.</p></body></html>');
+        //response header
+        if(req.method == "DELETE") {
+            res.writeHead(200, {'Content-Type': 'text/html'});
+            res.end('<h1>DELETE, Method!</h1>')
+        }
+    }
+    else if(req.url == "/user/friends"){
+        // res.write('<html><body><p>Comp Sci.</p></body></html>');
+        //response header
+        if(req.method == "GET"){
+            res.writeHead(200, {'Content-Type': 'text/html'});
+            res.end('<h1>GET, Method!</h1>')
+        }
+        else if(req.method == "POST"){
+            res.writeHead(200, {'Content-Type': 'text/html'});
+            res.end('<h1>POST, Method!</h1>')
+        }
+    }
+    else if(req.url == "/user/friends/search"){
+        // res.write('<html><body><p>Comp Sci.</p></body></html>');
+        //response header
+        if(req.method == "GET"){
+            res.writeHead(200, {'Content-Type': 'text/html'});
+            res.end('<h1>GET, Method!</h1>')
+        }
+    }
+    else if(req.url == "/user/friends/searchbyid"){
+        // res.write('<html><body><p>Comp Sci.</p></body></html>');
+        //response header
+        if(req.method == "GET"){
+            res.writeHead(200, {'Content-Type': 'text/html'});
+            res.end('<h1>GET, Method!</h1>')
+        }
+    }
+    else if(req.url == "/profilesettings/userstatus"){
+        // res.write('<html><body><p>Comp Sci.</p></body></html>');
+        //response header
+        if(req.method == "GET"){
+            res.writeHead(200, {'Content-Type': 'text/html'});
+            res.end('<h1>GET, Method!</h1>')
+        }
+    }
+    else if(req.url == "/chatroom"){
+        // res.write('<html><body><p>Comp Sci.</p></body></html>');
+        //response header
+        if(req.method == "GET"){
+            res.writeHead(200, {'Content-Type': 'text/html'});
+            res.end('<h1>GET, Method!</h1>')
+        }
+    }
+    else if(req.url == "/chatroom/create"){
+        // res.write('<html><body><p>Comp Sci.</p></body></html>');
+        //response header
+        if(req.method == "GET"){
+            res.writeHead(200, {'Content-Type': 'text/html'});
+            res.end('<h1>GET, Method!</h1>')
+        }
+        else if(req.method == "POST"){
+            res.writeHead(200, {'Content-Type': 'text/html'});
+            res.end('<h1>POST, Method!</h1>')
+        }
+    }
+    else if(req.url == "/chatroom/deletechatroom"){
+        // res.write('<html><body><p>Comp Sci.</p></body></html>');
+        //response header
+        if(req.method == "DELETE") {
+            res.writeHead(200, {'Content-Type': 'text/html'});
+            res.end('<h1>DELETE, Method!</h1>')
+        }
+    }
+    else if(req.url == "/searchchatroombyname"){
+        // res.write('<html><body><p>Comp Sci.</p></body></html>');
+        //response header
+        if(req.method == "GET"){
+            res.writeHead(200, {'Content-Type': 'text/html'});
+            res.end('<h1>GET, Method!</h1>')
+        }
+    }
+    else if(req.url == "/searchchatroombyid"){
+        // res.write('<html><body><p>Comp Sci.</p></body></html>');
+        //response header
+        if(req.method =  = "GET"){
+            res.writeHead(200, {'Content-Type': 'text/html'});
+            res.end('<h1>GET, Method!</h1>')
+        }
+    }
+    else if(req.url == "/chatroom/getinvite"){
+        // res.write('<html><body><p>Comp Sci.</p></body></html>');
+        //response header
+        if(req.method == "GET"){
+            res.writeHead(200, {'Content-Type': 'text/html'});
+            res.end('<h1>GET, Method!</h1>')
+        }
+    }
+    else if(req.url == "/chatroom/settings"){
+        // res.write('<html><body><p>Comp Sci.</p></body></html>');
+        //response header
+        if(req.method == "GET"){
+            res.writeHead(200, {'Content-Type': 'text/html'});
+            res.end('<h1>GET, Method!</h1>')
+        }
+        else if(req.method == "POST"){
+            res.writeHead(200, {'Content-Type': 'text/html'});
+            res.end('<h1>POST, Method!</h1>')
+        }
+    }
+    else if(req.url == "/subscription"){
+        // res.write('<html><body><p>Comp Sci.</p></body></html>');
+        //response header
+        if(req.method == "GET"){
+            res.writeHead(200, {'Content-Type': 'text/html'});
+            res.end('<h1>GET, Method!</h1>')
+        }
+        else if(req.method == "POST"){
+            res.writeHead(200, {'Content-Type': 'text/html'});
+            res.end('<h1>POST, Method!</h1>')
+        }
+    }
+    else if(req.url == "/subscription/purchase"){
+        // res.write('<html><body><p>Comp Sci.</p></body></html>');
+        //response header
+        if(req.method == "GET"){
+            res.writeHead(200, {'Content-Type': 'text/html'});
+            res.end('<h1>GET, Method!</h1>')
+        }
+        else if(req.method == "POST"){
+            res.writeHead(200, {'Content-Type': 'text/html'});
+            res.end('<h1>POST, Method!</h1>')
+        }
+    }
+    else if(req.url == "/subscription/cancel"){
+        // res.write('<html><body><p>Comp Sci.</p></body></html>');
+        //response header
+        if(req.method == "GET"){
+            res.writeHead(200, {'Content-Type': 'text/html'});
+            res.end('<h1>GET, Method!</h1>')
+        }
+        else if(req.method == "POST"){
+            res.writeHead(200, {'Content-Type': 'text/html'});
+            res.end('<h1>POST, Method!</h1>')
+        }
+    }
+    else if(req.url == "/profile/settings"){
+        // res.write('<html><body><p>Comp Sci.</p></body></html>');
+        //response header
+        if(req.method == "GET"){
+            res.writeHead(200, {'Content-Type': 'text/html'});
+            res.end('<h1>GET, Method!</h1>')
+        }
+        else if(req.method == "POST"){
+            res.writeHead(200, {'Content-Type': 'text/html'});
+            res.end('<h1>POST, Method!</h1>')
+        }
+    }
+    else if(req.url == "/friends/blocks"){
+        // res.write('<html><body><p>Comp Sci.</p></body></html>');
+        //response header
+        if(req.method == "GET"){
+            res.writeHead(200, {'Content-Type': 'text/html'});
+            res.end('<h1>GET, Method!</h1>')
+        }
+        else if(req.method == "POST"){
+            res.writeHead(200, {'Content-Type': 'text/html'});
+            res.end('<h1>POST, Method!</h1>')
+        }
+    }
+    else if(req.url == "/calendar"){
+        // res.write('<html><body><p>Comp Sci.</p></body></html>');
+        //response header
+        if(req.method == "GET"){
+            res.writeHead(200, {'Content-Type': 'text/html'});
+            res.end('<h1>GET, Method!</h1>')
+        }
+    }
+    else if(req.url == "/calendar/eventadd"){
+        // res.write('<html><body><p>Comp Sci.</p></body></html>');
+        //response header
+        if(req.method == "GET"){
+            res.writeHead(200, {'Content-Type': 'text/html'});
+            res.end('<h1>GET, Method!</h1>')
+        }
+    }
+    else if(req.url == "/calendar/eventedit"){
+        // res.write('<html><body><p>Comp Sci.</p></body></html>');
+        //response header
+        if(req.method == "GET"){
+            res.writeHead(200, {'Content-Type': 'text/html'});
+            res.end('<h1>GET, Method!</h1>')
+        }
+    }
+    else if(req.url == "/calendar/eventdelete"){
+        // res.write('<html><body><p>Comp Sci.</p></body></html>');
+        //response header
+        if(req.method == "GET"){
+            res.writeHead(200, {'Content-Type': 'text/html'});
+            res.end('<h1>GET, Method!</h1>')
         }
     }
     else
