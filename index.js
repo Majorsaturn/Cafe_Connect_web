25524a67330a1f758395cc228c6bc0ad8fb523c8
Majorsaturn <nicoanovak@gmail.com>
2024-10-07 13:23:05 -0400

    rough skeleton revision

--- index.js
+++ index.js
@@ -1,7 +1,5 @@
 var http = require('http');
 var server = http.createServer(function (req, res) {
-    res.statusCode = 200
-
 
 
     if (req.url == '/') {
@@ -272,3 +270,28 @@ var server = http.createServer(function (req, res) {
 server.listen(5000);
 
 console.log('node.js web server at port 5000 is running..')
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const uri = "mongodb+srv://nicoanovak:Xw7us3yzSyxXVGTW@cafeconnect1.pg0cb.mongodb.net/?retryWrites=true&w=majority&appName=cafeconnect1";
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+    serverApi: {
+        version: ServerApiVersion.v1,
+        strict: true,
+        deprecationErrors: true,
+    }
+});
+
+async function run() {
+    try {
+        // Connect the client to the server    (optional starting in v4.7)
+        await client.connect();
+        // Send a ping to confirm a successful connection
+        await client.db("admin").command({ ping: 1 });
+        console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    } finally {
+        // Ensures that the client will close when you finish/error
+        await client.close();
+    }
+}
+run().catch(console.dir);
